6ceef34058cad572d36b5dd73f9b88eb8de1a5f9
Nitesh-Sachde <202412083@daiict.ac.in>
2025-11-19 13:26:28 +0530

    Deployment

--- server/server.js
+++ server/server.js
@@ -39,7 +39,7 @@ app.use((req, res, next) => {
 app.use(express.static(path.join(__dirname, "public")));
 
 app.use(morgan("dev"));
-app.use(cors());
+app.use(cors({ origin: "*" }));
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(clerkMiddleware());
